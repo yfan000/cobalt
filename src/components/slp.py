b6807762817f08058b36ea71df51c7c85126eb22
Narayan Desai <desai@mcs.anl.gov>
2007-10-08 18:41:38 +0000

    Pull old code out of slp wrapper

--- src/components/slp.py
+++ src/components/slp.py
@@ -5,8 +5,6 @@ __revision__ = '$Revision$'
 from Cobalt.Components.slp import TimingServiceLocator
 from Cobalt.Components.base import run_component
 
-try:
-    slp = TimingServiceLocator()
-    run_component(slp, register=False)
-except KeyboardInterrupt:
-    pass
+slp = TimingServiceLocator()
+run_component(slp, register=False, trace=True)
+
