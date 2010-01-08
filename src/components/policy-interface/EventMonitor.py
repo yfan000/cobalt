56f90f3a84421b0c42eef4bb7089aaf8fd33fb94
Harish Naik <hnaik@mcs.anl.gov>
2010-01-08 20:09:58 +0000

    some more fixes to get it working

--- src/components/policy-interface/EventMonitor.py
+++ src/components/policy-interface/EventMonitor.py
@@ -4,28 +4,31 @@ import time, sys
 from FTBEventPolicy import *
 
 class EventMonitor():
+    ftbEventPolicy = None
     policyDefinitions = {}
 
     def __init__(self):
-	self.doRegister()
+	sHandle = self.doRegister()
+	self.startMonitor(sHandle)
 
     def doRegister(self):
-	fea = FTBEventPolicy()
-	sHandle = fea.register('0.5',
+	self.ftbEventPolicy = FTBEventPolicy()
+	sHandle = self.ftbEventPolicy.register('0.5',
                        'FTB.APP01.ES01',
                        'handler-01',
                        '0',
                        'FTB_SUBSCRIPTION_POLLING',
                        0)
+	return sHandle
 
-    def startMonitor(self):
-	self.policyDefinitions = fea.getPolicyDefinitions()
+    def startMonitor(self, sHandle):
+	self.policyDefinitions = self.ftbEventPolicy.getPolicyDefinitions()
 
 	while(True):
-	    cEvent, ret = fea.pollEvent(sHandle)
+	    cEvent, ret = self.ftbEventPolicy.pollEvent(sHandle)
 	    if ret != FTB_SUCCESS:
 		continue
-	    eventAttrib = policyDefinitions[cEvent.event_name]
+	    eventAttrib = self.policyDefinitions[cEvent.event_name]
 	    print 'Event-Type: ', eventAttrib.type, 
 	    print ' Caught: ', cEvent.event_name
 
