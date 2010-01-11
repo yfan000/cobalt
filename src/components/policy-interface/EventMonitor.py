57f355597e8eade90ac33c3893dc4b8d3dd86023
Harish Naik <hnaik@mcs.anl.gov>
2010-01-11 23:24:43 +0000

    processTimedTypeEvent works

--- src/components/policy-interface/EventMonitor.py
+++ src/components/policy-interface/EventMonitor.py
@@ -37,42 +37,47 @@ class EventMonitor():
 		self.processSingleTypeEvent(eventAttrib)
 
     def processSingleTypeEvent(self, ea):
-	print 'Event: %s, Type: %s, Action: %s' % (
+	print 'DEBUG: Event: %s, Type: %s, Action: %s' % (
 		        ea.name, ea.type, ea.action)
 
 
     def processMultipleTypeEvent(self, ea, ce):
 	multipleCountThreshold = int(ea.count)
 	if ((int(ce.seqnum) % multipleCountThreshold) == 0):
-            print 'Received event %s of type \'%s\' from client \'%s\' on host: %s!' % (
+            print 'DEBUG: Received event %s of type \'%s\' from client \'%s\' on host: %s!' % (
 		        ea.name, ea.type, ce.client_name, ce.incoming_src.hostname),
-            print 'Application threshold reached. Taking action: %s' % ( ea.action )
-            print 'client_jobid: %s, client_extension: %s, payload: %s' % (
+            print 'DEBUG: Application threshold reached. Taking action: %s' % ( ea.action )
+            print 'DEBUG: client_jobid: %s, client_extension: %s, payload: %s' % (
 			ce.client_jobid, ce.client_extension, ce.event_payload)
-            print 'PID start_time: %s, PID: %s' % (
+            print 'DEBUG: PID start_time: %s, PID: %s' % (
 			ce.incoming_src.pid_starttime, ce.incoming_src.pid)
 	
 
     timedEventMap = {}
-    prevTime = None
+#    prevTime = None
     def processTimedTypeEvent(self, ea, ce):
 	eventKey = ce.incoming_src.hostname + '-' + str(ce.incoming_src.pid)
+        curTime = time.time()
 #	print eventKey
 	
 	if not eventKey in self.timedEventMap:
-#             print 'Event: %s, Type: %s, Action: %s, Time: %s' % (
-# 		        ea.name, ea.type, ea.action, ea.time)
-            curTime = time.time()
-            if self.prevTime != None:
-		deltaTime = curTime - self.prevTime
-		self.prevTime = curTime
-		deltaMiliSec = deltaTime * 1000
-		if deltaMiliSec < int(ea.time):
-                    print "*** At %d milisecs ***" % (deltaMiliSec)
-		else:
-                    print 'Caught but safe(%d msec)' % (deltaMiliSec)
+            print 'DEBUG: First occurence of \'%s\'' % (eventKey)
+            self.timedEventMap.update({eventKey: curTime})
+	else:
+            prevTime = self.timedEventMap[eventKey]
+            deltaTime = curTime - self.timedEventMap[eventKey]
+            self.timedEventMap[eventKey] = curTime
+            deltaMiliSec = deltaTime * 1000
+            if deltaMiliSec < int(ea.time):
+                print 'DEBUG: *** At %d msecs ***' % (deltaMiliSec),
+		print '. Taking action: %s.' % (ea.action) ,
+		print ' for eventKey: %s' % (eventKey)
             else:
-		self.prevTime = curTime
+                print 'DEBUG: Caught event above threshold(%d msec). No action required.' % (
+				deltaMiliSec), 
+
+            print '. Threshold: %s msec' % (ea.time)
+
 
 if __name__=='__main__':
 	EventMonitor()
