2516e10222dadbfebf354f2d5da3ff541c53f251
Paul Rich <richp@alcf.anl.gov>
2014-10-03 14:58:55 -0500

    Adding place for stub interface and related files for accounting.

---
+++ src/lib/RTAccounting/stub_interface.py
@@ -0,0 +1,11 @@
+"""Accounting Interface null implementation
+
+This is a do-nothing stub useful for testing, or running against accounting systems that do not implement real-time accounting.
+
+
+"""
+
+class AccountingStub(object):
+
+    def fetch_job_info(self, jobdata, timestamp):
+        pass
